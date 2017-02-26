20ef5cd48dbda3c3e27364a05caaec8e3202a901
Kushtrim <kushtrim.abdiu.16@gmail.com>
2017-02-26 22:57:13 +0100

    A class that reads the API data

--- LibraryApp/Models/apiData.cs
+++ LibraryApp/Models/apiData.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using System.Net;
+
 namespace LibraryApp
 {
 	public class apiData
 	{
-		public apiData()
+		/**
+		 *  the API endpoint
+		 */
+		private string API_URL = "http://www.contribe.se/arbetsprov-net/books.json";
+
+		public apiData() {
+			
+		}
+
+		/**
+		 * getBooksFromAPI - reads the data from the remote API
+		 * @return json - the books in json format
+		 */
+		public string getBooksFromAPI()
 		{
+			try {
+				var webClient = new WebClient();
+				return webClient.DownloadString(API_URL);
+			}
+			catch (Exception ex) {
+				return "error";
+			}
 		}
 	}
 }
