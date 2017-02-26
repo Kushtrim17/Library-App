366c04a74c8faa2e10864e3358642147f9c34318
Kushtrim <kushtrim.abdiu.16@gmail.com>
2017-02-27 00:57:26 +0100

    Added functionallity to get all the books and to remove a book from the book list

--- LibraryApp/Models/Books.cs
+++ LibraryApp/Models/Books.cs
@@ -6,6 +6,27 @@ namespace Kushtrim.Models
 {
 	public class Books
 	{
+		/**
+		 * books - a list that holds instances of the class Book
+		 */
 		public IList<Book> books;
+
+		/**
+		 * getAllBooks
+		 * @return books| IList
+		 */
+		public IList<Book> getAllBooks()
+		{
+			return books;
+		}
+
+		/**
+		 * removeBook - removes the given book from the book list
+		 * @param book|Book
+		 */
+		public void removeBook(Book book)
+		{
+			books.Remove(book);
+		}
 	}
 }
