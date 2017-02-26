3b4525ef66dadf0093c784c47c6339d2fd8f2f72
Kushtrim <kushtrim.abdiu.16@gmail.com>
2017-02-26 21:45:57 +0100

    Adding the navigation bar and creating the Book and Books class to parse the JSON object from the given URL

---
+++ LibraryApp/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.ClientServices;
+using System.Net;
+using System.Web.Script.Serialization;
+using System.Web;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace Kushtrim.Models
+{
+	public class Book
+	{
+		public string title;
+
+		public string author;
+
+		public string price;
+
+		public string inStock;
+	}
+}
---
+++ LibraryApp/Models/Books.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Kushtrim.Models;
+
+namespace Kushtrim.Models
+{
+	public class Books
+	{
+		public IList<Book> books;
+	}
+}
