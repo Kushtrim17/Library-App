75ab5d4b693549c28b11c02c28acd6d406a361f9
Kushtrim <kushtrim.abdiu.16@gmail.com>
2017-02-27 03:42:45 +0100

    Handle add to card

--- LibraryApp/Controllers/HomeController.cs
+++ LibraryApp/Controllers/HomeController.cs
@@ -11,26 +11,19 @@ namespace LibraryApp.Controllers
 {
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
-		{
-			var data = new apiData();
-			var jsonBooks = data.getBooksFromAPI();
-			var books = JsonConvert.DeserializeObject<Books>(jsonBooks);
-
-			return View(books);
-		}
-
-		public ActionResult Search(string searchTerm)
+		public ActionResult Index(string searchTerm = "")
 		{
 			var data = new apiData();
 			var jsonBooks = data.getBooksFromAPI();
 			var books = JsonConvert.DeserializeObject<Books>(jsonBooks);
 
 			//if the search is empty then we dont filter
-			if (searchTerm.Length > 0) {
+			if (searchTerm.Length > 0)
+			{
 				//after we got all the books from the API we filter out the ones 
 				//that dont match the searched term
-				foreach (Book book in books.getAllBooks().ToList()) {
+				foreach (Book book in books.getAllBooks().ToList())
+				{
 					if (!book.isSelectedBook(searchTerm)) {
 						books.removeBook(book);
 					}
@@ -38,6 +31,29 @@ namespace LibraryApp.Controllers
 			}
 
 			return View(books);
+		}
+
+		public RedirectResult AddToCard(string bookTitle, string bookAuthor, int bookinStock)
+		{
+			if (bookinStock > 1) {
+				//we can buy the book 
+				return new RedirectResult("/Home/AddedToCard?title=" + bookTitle);
+			}
+			else {
+				//we should redirect to the view that handles
+				return new RedirectResult("/Home/OutOfStock?title=" + bookTitle);
+			}
+
+		}
+
+		public ActionResult OutOfStock()
+		{
+			return View();
+		}
+
+		public ActionResult AddedToCard()
+		{
+			return View();
 		}
 	}
 }
