2ee96bc77a1f053187f6a0d99a35f6021ba5b77f
Kushtrim <kushtrim.abdiu.16@gmail.com>
2017-03-01 19:06:31 +0100

    Added some commenting

--- LibraryApp/Models/Book.cs
+++ LibraryApp/Models/Book.cs
@@ -12,13 +12,31 @@ namespace Kushtrim.Models
 {
 	public class Book
 	{
+		/**
+		 * the title
+		 */
 		public string title;
 
+		/**
+		 * the author
+		 */
 		public string author;
 
+		/**
+		 * the price of the given book
+		 */
 		public string price;
 
-		public string inStock;
+		/**
+		 *  how many books we have in stock
+		 */
+		public int inStock;
+
+		/**
+		 * variable to control the amount of times
+		 * a book appears in the shopping list
+		 */
+		public int amount;
 
 		/**
 		 * isSelectedBook - checks if the current book is part of the search
