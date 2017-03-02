a7307447c916a2e4eadcbe003ba1cb5dcfa73a58
Kushtrim <kushtrim.abdiu.16@gmail.com>
2017-03-02 04:50:27 +0100

    Added asynchrnous handling of the Http Request as specified in the assignment description

---
+++ LibraryApp/Models/BookstoreService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Kushtrim.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace LibraryApp
+{
+	public class BookstoreService
+	{
+		/**
+		 *  the API endpoint
+		 */
+		private const string API_URL = "http://www.contribe.se/arbetsprov-net/books.json";
+
+		/**
+		 *  GetBooksAsync - gets the books from the API asynchronously
+		 *  @return Task<string>
+		 */
+		public static async Task<string> GetBooksAsync()
+		{
+			try {
+				HttpClient client = new HttpClient();
+				string json = await client.GetStringAsync(API_URL);
+
+				return json;
+			}
+			catch (HttpRequestException ex) {
+				Console.WriteLine(ex.Message);
+				return ex.Message;
+			}
+
+		}
+	}
+}
